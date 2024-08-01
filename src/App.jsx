4389560c41a6410270163603925672920faf669c
Monika-Lis <lis.monikak@gmail.com>
2024-08-01 22:56:54 +0100

    change to app.js to show components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
+import React from 'react';
+import { HomePage } from './pages/HomePage/Home';
+import { WelcomePage } from './pages/WelcomePage/Welcome';
+import { Footer } from './components/Footer/Footer';
+
 export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HomePage />
+      <WelcomePage />
+      <Footer />
     </div>
   );
 };
