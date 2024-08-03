d2b4207dc07432490402642baf6d701489e7fdb0
KrolLew23 <aniabandola@gmail.com>
2024-08-03 22:08:21 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { HomePage } from './pages/HomePage/Home';
 import { WelcomePage } from './pages/WelcomePage/Welcome';
 import { Footer } from './components/Footer/Footer';
+import { Register } from './pages/Auth/Register/Register';
 
 export const App = () => {
   return (
-    <div>
-      <HomePage />
-      <WelcomePage />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/welcome" element={<WelcomePage />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 };
