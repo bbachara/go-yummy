f2ac9acce4dfb97ea9ccb72d7ba0cb22d3f666fb
MikolajBlajek <128635948+MikolajBlajek@users.noreply.github.com>
2024-08-06 21:24:24 +0200

    test
    Test

--- src/pages/HomePage/components/PreviewCategory.jsx
+++ src/pages/HomePage/components/PreviewCategory.jsx
@@ -1,59 +1,56 @@
-// import Notiflix from 'notiflix';
-// import { useEffect, useState } from 'react';
-// import { fetchPopularCategories } from '../../../api/homePageAPI';
-// import css from '../Home.module.css';
-// import { useNavigate } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
+import Notiflix from 'notiflix';
+import { useEffect, useState } from 'react';
+import { fetchPopularCategories } from '../../../api/homePageAPI';
+import css from '../Home.module.css';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-// export const PopularCategories = () => {
-//   const [popularCategories, setPopularCategories] = useState([]);
-//   const navigate = useNavigate();
-//   const token = useSelector(state => state.auth.token);
-//   useEffect(() => {
-//     const getCategories = async () => {
-//       const getData = await getCategoryList(token);
-//       setPopularCategories(getData);
-//     };
-//     getCategories();
-//   }, [token]);
+export const PopularCategories = () => {
+  const [popularCategories, setPopularCategories] = useState([]);
+  const navigate = useNavigate();
+  const token = useSelector(state => state.auth.token);
+  useEffect(() => {
+    const getCategories = async () => {
+      const getData = await getCategoryList(token);
+      setPopularCategories(getData);
+    };
+    getCategories();
+  }, [token]);
 
-//   useEffect(() => {
-//     fetchPopularCategories()
-//       .then(({ data }) => {
-//         setCategories(data.results);
-//       })
-//       .catch(() => {
-//         Notiflix.Notify.failure('Error fetching data');
-//       });
-//   }, []);
+  useEffect(() => {
+    fetchPopularCategories()
+      .then(({ data }) => {
+        setCategories(data.results);
+      })
+      .catch(() => {
+        Notiflix.Notify.failure('Error fetching data');
+      });
+  }, []);
 
-//   return (
-//     <div className={css.previewCategories}>
-//       <previewCategoryCard
-//         title="Breakfast"
-//         category="breakfastRecipes"
-//       ></previewCategoryCard>{' '}
-//       <previewCategoryCard
-//         title="Miscellaneous"
-//         category="miscellaneousRecipes"
-//       ></previewCategoryCard>
-//       <previewCategoryCard
-//         title="Chicken"
-//         category="chickenRecipes"
-//       ></previewCategoryCard>
-//       <previewCategoryCard
-//         title="Desserts"
-//         category="dessertRecipes"
-//       ></previewCategoryCard>
-//       <button className={css.previewCategoriesOtherCategories}>
-//         Other categories
-//       </button>
-//     </div>
-//   );
-// }; 
-
-//Zakomentowany kod u góry ponieważ wywala błąd//
-
+  return (
+    <div className={css.previewCategories}>
+      <previewCategoryCard
+        title="Breakfast"
+        category="breakfastRecipes"
+      ></previewCategoryCard>{' '}
+      <previewCategoryCard
+        title="Miscellaneous"
+        category="miscellaneousRecipes"
+      ></previewCategoryCard>
+      <previewCategoryCard
+        title="Chicken"
+        category="chickenRecipes"
+      ></previewCategoryCard>
+      <previewCategoryCard
+        title="Desserts"
+        category="dessertRecipes"
+      ></previewCategoryCard>
+      <button className={css.previewCategoriesOtherCategories}>
+        Other categories
+      </button>
+    </div>
+  );
+};
 
 // return (
 //   <div className={css.previewCategories}>
