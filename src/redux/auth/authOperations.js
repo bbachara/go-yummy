9efcf80880f180323550457005a182d3f24fcac1
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2024-08-08 21:15:38 +0200

    Create authOperations.js

---
+++ src/redux/auth/authOperations.js
@@ -0,0 +1,66 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+const token = {
+  set(token) {
+    instance.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    instance.defaults.headers.common.Authorization = '';
+  },
+};
+
+export const signUp = createAsyncThunk(
+  'auth/signup',
+  async (credentials, thunkAPI) => {
+    try {
+      const { data } = await instance.post('/auth/register', credentials);
+      token.set(data.token);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logIn = createAsyncThunk(
+  'auth/login',
+  async (credentials, thunkAPI) => {
+    try {
+      const { data } = await instance.post('/auth/login', credentials);
+      token.set(data.token);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+  try {
+    await instance.post('/auth/logout');
+    token.unset();
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+export const refreshUser = createAsyncThunk(
+  'auth/refresh',
+  async (_, thunkAPI) => {
+    const persistedToken = thunkAPI.getState().auth.token;
+    if (persistedToken === null)
+      return thunkAPI.rejectWithValue('Unable to fetch user');
+    try {
+      token.set(persistedToken);
+      const { data } = await instance.get('/auth/current');
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
