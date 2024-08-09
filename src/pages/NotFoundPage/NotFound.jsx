2971782ac88cc90ba35afa7665f1cf4c1109b71b
KrolLew23 <aniabandola@gmail.com>
2024-08-09 20:06:38 +0200

    notfound

--- src/pages/NotFoundPage/NotFound.jsx
+++ src/pages/NotFoundPage/NotFound.jsx
@@ -1,45 +1,55 @@
+import React from 'react';
+import notFoundMobile from '../../assets/NotFoundPage/404-page-not-found-with-people-connecting-a-plug-mobile.png';
+import notFoundTablet from '../../assets/NotFoundPage/404-page-not-found-with-people-connecting-a-plug-tablet.png';
+import notFoundDesktop from '../../assets/NotFoundPage/404-page-not-found-with-people-connecting-a-plug-desktop.png';
+import notFoundMobile2 from '../../assets/NotFoundPage/404-page-not-found-with-people-connecting-a-plug-mobile@x2.png';
+import notFoundTablet2 from '../../assets/NotFoundPage/404-page-not-found-with-people-connecting-a-plug-tablet@x2.png';
+import notFoundDesktop2 from '../../assets/NotFoundPage/404-page-not-found-with-people-connecting-a-plug-desktop@x2.png';
 import css from './NotFound.module.css';
 
 export const NotFound = () => {
   return (
     <div className={css.container}>
       <div className={css.wrapper}>
-        <img
-          className={css.imgMobile}
-          srcSet="
-                ../../assets/NotFoundPage/404-page-not-found-with-people-connecting-a-plug-mobile.png    259w,
-                ../../assets/NotFoundPage/404-page-not-found-with-people-connecting-a-plug-mobile@x2.png 518w
-              "
-          sizes="(min-width: 320px) 320px"
-          src="../../assets/NotFoundPage/404-page-not-found-with-people-connecting-a-plug-mobile.png"
-          alt="people connecting a plug"
-          width="259"
-          height="170"
-        />
-        <img
-          className={css.imgTablet}
-          srcSet="
-                ../../assets/NotFoundPage/404-page-not-found-with-people-connecting-a-plug-tablet.png    498w,
-                ../../assets/NotFoundPage/404-page-not-found-with-people-connecting-a-plug-tablet@x2.png 996w
-              "
-          sizes="(min-width: 768px) 768px"
-          src="../../assets/NotFoundPage/404-page-not-found-with-people-connecting-a-plug-tablet.png"
-          alt="people connecting a plug"
-          width="498"
-          height="327"
-        />
-        <img
-          className={css.imgDesktop}
-          srcSet="
-                ../../assets/NotFoundPage/404-page-not-found-with-people-connecting-a-plug-desktop.png    1x,
-                ../../assets/NotFoundPage/404-page-not-found-with-people-connecting-a-plug-desktop@x2.png 2x
-              "
-          sizes="(min-width: 1200px) 1200px"
-          src="../../assets/NotFoundPage/404-page-not-found-with-people-connecting-a-plug-desktop.png"
-          alt="people connecting a plug"
-          width="498"
-          height="331"
-        />
+        <picture>
+          <source
+            media="(max-width: 767px)"
+            srcSet={`${notFoundMobile2} 2x, ${notFoundMobile} 1x`}
+          />
+          <img
+            className={css.imgMobile}
+            src={notFoundMobile}
+            alt="People connecting a plug"
+            width="259"
+            height="170"
+          />
+        </picture>
+        <picture>
+          <source
+            media="(min-width: 768px) and (max-width: 1199px)"
+            srcSet={`${notFoundTablet2} 2x, ${notFoundTablet} 1x`}
+          />
+          <img
+            className={css.imgTablet}
+            src={notFoundTablet}
+            alt="People connecting a plug"
+            width="498"
+            height="327"
+          />
+        </picture>
+        <picture>
+          <source
+            media="(min-width: 1200px)"
+            srcSet={`${notFoundDesktop2} 2x, ${notFoundDesktop} 1x`}
+          />
+          <img
+            className={css.imgDesktop}
+            src={notFoundDesktop}
+            alt="People connecting a plug"
+            width="498"
+            height="331"
+          />
+        </picture>
         <div className={css.text}>
           <h2 className={css.h2}>We are sorry,</h2>
           <p className={css.p}>
