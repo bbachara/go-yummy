da1b87c79b3f2aa69a873c3cadb67d75a7f57fbe
Monika-Lis <lis.monikak@gmail.com>
2024-08-09 22:41:58 +0100

    start from start

--- src/components/PreviewCategory/PreviewCategory.jsx
+++ src/components/PreviewCategory/PreviewCategory.jsx
@@ -1,70 +1,7 @@
-import css from './PreviewCategory.module.css';
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { fetchCategories } from '../../redux/recipes/categories/operations';
-import { PreviewCategoryCard } from 'components/PreviewCategoryCard/PreviewCateogryCard';
+import { fetchRecipesByCategory } from 'api/homePageAPI';
 
-export const PopularCategories = () => {
-  const dispatch = useDispatch();
-  const [popularCategories, setPopularCategories] = useState([]);
-  const navigate = useNavigate();
-  // const token = useSelector(state => state.auth.token);
-
-  const getCategories = async () => {
-    const dataPopularCategories = await fetchCategories();
-    setPopularCategories(dataPopularCategories);
-  };
-
-  useEffect(() => {
-    dispatch(getCategories());
-  }, [dispatch]);
-
-  const breakfastCateogry = popularCategories.filter(
-    popularCategory => popularCategory.category === 'Breakfast'
-  );
-
-  const miscellaneousCateogry = popularCategories.filter(
-    popularCategory => popularCategory.category === 'Miscellaneous'
-  );
-
-  const chickenCategory = popularCategories.filter(
-    popularCategory => popularCategory.category === 'Chicken'
-  );
-
-  const dessertCategory = popularCategories.filter(
-    popularCategory => popularCategory.category === 'Dessert'
-  );
-
-  const otherCategoriesClick = () => {
-    navigate(`/recipes/categories/`);
-  };
-
-  console.log(setPopularCategories);
-
-  return (
-    <div className={css.previewCategories}>
-      {breakfastCateogry && (
-        <PreviewCategoryCard title="Breakfast" category="breakfastCategory" />
-      )}
-      {miscellaneousCateogry && (
-        <PreviewCategoryCard
-          title="Miscellaneous"
-          category="miscellaneousCateogry"
-        />
-      )}
-      {chickenCategory && (
-        <PreviewCategoryCard title="Chicken" category="chickenCategory" />
-      )}
-      {dessertCategory && (
-        <PreviewCategoryCard title="Desserts" category="dessertCategory" />
-      )}
-      <button
-        className={css.previewCategoriesOtherCategories}
-        onClick={otherCategoriesClick}
-      >
-        Other categories
-      </button>
-    </div>
-  );
+export const PreviewCategory = () => {
+  const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2YjE0YTkwZTEwNjA3YjQ3MGFmYjA5OSIsImVtYWlsIjoibWlrb2xhamJsYWpla0BnbWFpbC5jb20iLCJpYXQiOjE3MjMyMjYzMDIsImV4cCI6MTcyMzMzNDMwMn0.pyv0SUpeRc31UbPstzyC_32rbeaS8C1iH8vknG76Qyo';
+  fetchRecipesByCategory('BEEF', token);
 };
--- src/components/PreviewCategoryCard/PreviewCateogryCard.jsx
+++ src/components/PreviewCategoryCard/PreviewCateogryCard.jsx
@@ -1,38 +1,4 @@
-import { useNavigate } from 'react-router-dom';
-import css from './PreviewCategoryCard.module.css';
-import { PreviewRecipeCard } from '../PreviewRecipeCard/PreviewRecipeCard';
 
-export const PreviewCategoryCard = ({ title, category }) => {
-  const navigate = useNavigate();
 
-  const GoToRecipeClick = recipeId => {
-    navigate(`/recipes/${recipeId}`);
-  };
 
-  const SeeAllClick = () => {
-    navigate(`/recipes/categories/${title}`);
-  };
 
-  return (
-    <div className={css.popularCategoryUl}>
-      <div className={css.popularCategoryLi}>
-        <div className={css.popularCategoryHeader}>
-          <h3 className={css.popularCategoryHeading}>{title}</h3>
-        </div>
-        <ul className={css.popularRecipeUl}>
-          {category.map(recipe => (
-            <PreviewRecipeCard
-              className={css.popularRecipeLi}
-              key={recipe._id}
-              recipe={recipe}
-              onClick={() => GoToRecipeClick(recipe._id)}
-            />
-          ))}
-        </ul>
-        <button className={css.seeAll} onClick={SeeAllClick}>
-          See all
-        </button>
-      </div>
-    </div>
-  );
-};
