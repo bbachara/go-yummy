c06064a8e230087680d309a1b654bf3b8ec9f897
Monika-Lis <lis.monikak@gmail.com>
2024-08-11 20:47:21 +0100

    commented

--- src/pages/FavoritesPage/CardFavorite/CardFavorite.js
+++ src/pages/FavoritesPage/CardFavorite/CardFavorite.js
@@ -1,37 +1,37 @@
-import { Link, useLocation } from 'react-router-dom';
+// import { Link, useLocation } from 'react-router-dom';
 
-import {
-  Card,
-  ImgWrapper,
-  ImgRecipe,
-  TextRecipe,
-  TitleNameRecipe,
-  WrapperImgTrashLogo,
-  WrapperTimeBtn,
-  WrapperContent,
-  TimeP,
-  WrapperImgBtnRecipe,
-  WrapperTextRecipe,
-  TrashLogo,
-} from './CardFavorite.styled.js';
+// import {
+//   Card,
+//   ImgWrapper,
+//   ImgRecipe,
+//   TextRecipe,
+//   TitleNameRecipe,
+//   WrapperImgTrashLogo,
+//   WrapperTimeBtn,
+//   WrapperContent,
+//   TimeP,
+//   WrapperImgBtnRecipe,
+//   WrapperTextRecipe,
+//   TrashLogo,
+// } from './CardFavorite.styled.js';
 
-export default function CardFavorite({ item, handleDelete }) {
-  const { title, time, instructions, thumb, _id } = item;
-  const location = useLocation();
+// export default function CardFavorite({ item, handleDelete }) {
+//   const { title, time, instructions, thumb, _id } = item;
+//   const location = useLocation();
 
-  return (
-    <Card>
-      <ImgWrapper>
-        <Link to={`/recipe/${_id}`} state={{ from: location }}>
-          <ImgRecipe src={thumb} alt="trash" />
-        </Link>
-      </ImgWrapper>
-      <WrapperContent>
-        <TitleNameRecipe>{title}</TitleNameRecipe>
-        <WrapperTextRecipe>
-          <TextRecipe>{instructions}</TextRecipe>
-        </WrapperTextRecipe>
-      </WrapperContent>
-    </Card>
-  );
-}
+//   return (
+//     <Card>
+//       <ImgWrapper>
+//         <Link to={`/recipe/${_id}`} state={{ from: location }}>
+//           <ImgRecipe src={thumb} alt="trash" />
+//         </Link>
+//       </ImgWrapper>
+//       <WrapperContent>
+//         <TitleNameRecipe>{title}</TitleNameRecipe>
+//         <WrapperTextRecipe>
+//           <TextRecipe>{instructions}</TextRecipe>
+//         </WrapperTextRecipe>
+//       </WrapperContent>
+//     </Card>
+//   );
+// }
